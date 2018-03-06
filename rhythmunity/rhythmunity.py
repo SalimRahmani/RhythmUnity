610bf23c1bb7a01a8ccfb73c6e236b647f52d5f7
Salim Rahmani <SalimRahmani@outlook.com>
2018-03-06 22:19:07 +0000

    clean code

--- rhythmunity/rhythmunity.py
+++ rhythmunity/rhythmunity.py
@@ -1,20 +1,19 @@
+from __future__ import print_function
 import json
 from datetime import datetime
 from model import *
-from __future__ import print_function
-
 
 __author__ = 'Salim Rahmani'
 
 
-def loadJSON(file):
+def load_json(file):
     jsonfile = open(file)
     data = json.load(jsonfile)
     jsonfile.close()
     return data
 
 
-def loadsMembers(data):
+def load_members(data):
     members = []
     for fullname in data:
         memberSchedule = {"Monday": [], "Tuesday": [], "Wednesday": [], "Thursday": []}
@@ -22,21 +21,21 @@ def loadsMembers(data):
         for day in data[fullname]:
             for timeslot in data[fullname][day]:
                 timeslot_tokenized = timeslot.split(" - ")
-                timeslot = Timeslot(datetime.strptime(timeslot_tokenized[0], format), datetime.strptime(timeslot_tokenized[1], format))
+                timeslot = Timeslot(datetime.strptime(timeslot_tokenized[0], date_format), datetime.strptime(timeslot_tokenized[1], date_format))
                 memberSchedule[day].append(timeslot)
         member.setSchedule(memberSchedule)
         members.append(member)
     return members
 
 
-def loadsBands(data):
+def load_bands(data):
     bands = []
     for band_name in data:
         bandsMembers = []
         band = Band(band_name)
         for member_name in data[band_name]:
-            if getMember(member_name) is not None:
-                bandsMembers.append(members[getMember(member_name)])
+            if get_member(member_name) is not None:
+                bandsMembers.append(members[get_member(member_name)])
             else:
                 print(member_name + " doesn't exist in the Members list! Please re-check!")
         band.members.extend(bandsMembers)
@@ -44,22 +43,22 @@ def loadsBands(data):
     return bands
 
 
-def getMember(fullname):
+def get_member(fullname):
     for member in members:
         if(fullname == member.fullname):
             return members.index(member)
 
 
-def generalSchedule(members):
+def get_general_schedule(members):
     schedule = {"Monday": [], "Tuesday": [], "Wednesday": [], "Thursday": []}
     for member in members:
         for day in days:
             schedule[day].extend(member.schedule[day])
-    schedule = checkSchedule(schedule)
-    return freetimeschedule(schedule)
+    schedule = check_schedule(schedule)
+    return get_freetime_schedule(schedule)
 
 
-def checkSchedule(sched):
+def check_schedule(sched):
     for day in days:
         i = 0
         sched[day] = sorted(sched[day], key=lambda timeslot: (timeslot.start, timeslot.end))
@@ -75,22 +74,22 @@ def checkSchedule(sched):
     return sched
 
 
-def bandsSchedule(bands):
+def get_bands_schedule(bands):
     for band in bands:
         bandSchedule = {"Monday": [], "Tuesday": [], "Wednesday": [], "Thursday": []}
-        bandSchedule = generalSchedule(band.members)
+        bandSchedule = get_general_schedule(band.members)
         band.setSchedule(bandSchedule)
 
 
-def printschedule(sched):
+def print_schedule(sched):
     for day in days:
         print("\t" + day)
         for timeslot in sched[day]:
-            print("\t\t" + datetime.strftime(timeslot.start, format) + " - " + datetime.strftime(timeslot.end, format))
+            print("\t\t" + datetime.strftime(timeslot.start, date_format) + " - " + datetime.strftime(timeslot.end, date_format))
 
 
-def freetimeschedule(schedule):
-    timeslot = Timeslot(datetime.strptime("08:00 AM", format), datetime.strptime("11:45 PM", format))
+def get_freetime_schedule(schedule):
+    timeslot = Timeslot(datetime.strptime("08:00 AM", date_format), datetime.strptime("11:45 PM", date_format))
     freeschedule = {"Monday": [timeslot], "Tuesday": [timeslot], "Wednesday": [timeslot], "Thursday": [timeslot]}
     for day in days:
         i = 0
@@ -113,64 +112,37 @@ def freetimeschedule(schedule):
                     freeschedule[day].append(tms_one)
                     i -= 1
                 if (timeslot.start <= freeschedule[day][i].start) and (timeslot.end >= freeschedule[day][i].end):
-                    # remove
                     freeschedule[day].pop(i)
                     return 0
-                    # break
             i += 1
     return freeschedule
 
 
 if __name__ == '__main__':
-    # 0- Global Variables
-    format = "%I:%M %p"
+
+    date_format = "%I:%M %p"
     days = ["Monday", "Tuesday", "Wednesday", "Thursday"]
     schedule = {"Monday": [], "Tuesday": [], "Wednesday": [], "Thursday": []}
 
-# ------------------------------------------------------------------------------------------------------------------------
-# 1- Load All Members
-    # 1-1 Load JSON file
     print("-" * 80)
     print("Load the Json file: MembersSchedule.json")
-    data = loadJSON('memberschedule.json')
-    # 1-2 Initialize Members
-    print("Loading members: instance, schedule, and members list...\n")
-    members = loadsMembers(data)
-    '''
-    # 1-3 Print Each Member's schedule
-    print "Print Each Member's schedule"
-    for member in members:
-        print member.fullname
-        printschedule(member.schedule)
-    print "-" * 80
-    '''
+    data = load_json('memberschedule.json')
+    members = load_members(data)
+
     # 1-4 Generate the General Schedule
-    schedule = generalSchedule(members)
+    schedule = get_general_schedule(members)
     print("The General Schedule: ")
-    printschedule(schedule)
+    print_schedule(schedule)
     print("-" * 80)
 
-# ------------------------------------------------------------------------------------------------------------------------
-# 2- Load Bands
-    # 2-1 Load JSON file
     print("Load the Json file: Bands.json")
-    data = loadJSON('bands.json')
-    # 2-2 Initialize Bands
+    data = load_json('bands.json')
     print("Loading bands: instance, schedule, and members list...\n")
-    bands = loadsBands(data)
-    '''
-    # 2-3 Print the schedule of each band's member
-    for band in bands:
-        print band.name
-        for member in band.members:
-            print "\t" + member.fullname
-            printschedule(member.schedule)
-    print "-" * 80
-    '''
+    bands = load_bands(data)
+
     # 2-4 Generate Band Schedule based on members schedules
-    bandsSchedule(bands)
-    # 2-5 Print each band's schedule
+    get_bands_schedule(bands)
+
     for band in bands:
         print(band.name)
-        printschedule(band.schedule)
-# ------------------------------------------------------------------------------------------------------------------------
+        print_schedule(band.schedule)
